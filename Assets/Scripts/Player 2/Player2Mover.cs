1d67fe4dfcabc792a1f43bbca37baf30b584e67f
CodeIsCompiling <matthew.kostikov@gmail.com>
2025-09-20 09:14:08 -0700

    animations

--- Assets/Scripts/Player 2/Player2Mover.cs
+++ Assets/Scripts/Player 2/Player2Mover.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Player_2
@@ -8,6 +9,10 @@ namespace Player_2
         public int speed = 10;
         public float groundCheckDistance = 0.1f;
         public LayerMask groundLayer;
+        public Transform groundCheck;
+        public Vector2 groundCheckSize;
+        [SerializeField] private Vector3 halfExtents = new Vector3(0.5f, 0.5f, 0.5f);
+        
         private void FixedUpdate()
         {
             float moveInputX = 0f;
@@ -23,15 +28,36 @@ namespace Player_2
                 transform.localScale = new Vector3(1, 1, 1);
                 moveInputX = 1;
             }
-            
-            RaycastHit2D hit = Physics2D.Raycast(transform.position - new Vector3(0.65f, 2.05f, 0), Vector2.right, groundCheckDistance, groundLayer);
 
+            RaycastHit2D hit = Physics2D.BoxCast(groundCheck.position, groundCheckSize, 0f, Vector2.down, groundCheckDistance, groundLayer);
+            
             if (hit.collider != null && Input.GetKey(KeyCode.W))
             {
                 rb.linearVelocityY = 10;
             }
             rb.linearVelocityX = moveInputX * speed;
+            
         }
-        
+
+        /*private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+
+      
+            Vector3 dir = transform.forward;
+
+
+            Vector3 start = transform.position;
+            Vector3 end = start + dir * groundCheckDistance;
+
+
+            Gizmos.DrawLine(start, end);
+
+
+            Gizmos.DrawWireCube(start, halfExtents * 2);
+
+
+            Gizmos.DrawWireCube(end, halfExtents * 2);
+        }*/
     }
 }
